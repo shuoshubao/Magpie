ac6a0e598722a541cfbfeac47f1d12bffafed1e1
shuoshubao <759979885@qq.com>
2022-04-07 13:11:35 +0800

    feat: ipcRenderer ipcMain

--- react.config.js
+++ react.config.js
@@ -2,36 +2,37 @@
  * @Author: fangt11
  * @Date:   2021-11-24 10:50:57
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-06 14:02:58
+ * @Last Modified time: 2022-04-07 12:01:34
  * @Desc: 打包配置
  */
 
-module.exports = ({ isMac, isDevelopment, isProduction }) => {
-    // electron
-    return {
-        publicPath: isDevelopment ? '/':'https://storage.lianjia.com/tent/electron/dist/',
-        configureWebpack: {
-            externals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-                antd: 'antd',
-                moment: 'moment',
-                lodash: '_'
-            }
-        },
-        assets: {
-            css: ['https://file.ljcdn.com/bs/antd/4.18.7/dist/antd.min.css'],
-            js: [
-                'https://file.ljcdn.com/bs/lodash/4.17.21/lodash.min.js',
-                `https://file.ljcdn.com/bs/react/17.0.2/umd/${
-                    isDevelopment ? 'react.development.js' : 'react.production.min.js'
-                }`,
-                `https://file.ljcdn.com/bs/react-dom/17.0.2/umd/${
-                    isDevelopment ? 'react-dom.development.js' : 'react-dom.production.min.js'
-                }`,
-                'https://file.ljcdn.com/bs/moment/2.25.3/min/moment.min.js',
-                'https://file.ljcdn.com/bs/antd/4.18.7/dist/antd.min.js'
-            ]
-        }
-    };
-};
+module.exports = ({ isDevelopment, isProduction }) => {
+  // electron
+  return {
+    publicPath: isDevelopment ? '/' : 'https://storage.lianjia.com/tent/electron/dist/',
+    configureWebpack: {
+      target: 'electron-renderer',
+      externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        antd: 'antd',
+        moment: 'moment',
+        lodash: '_'
+      }
+    },
+    assets: {
+      css: ['https://file.ljcdn.com/bs/antd/4.18.7/dist/antd.min.css'],
+      js: [
+        'https://file.ljcdn.com/bs/lodash/4.17.21/lodash.min.js',
+        `https://file.ljcdn.com/bs/react/17.0.2/umd/${
+          isDevelopment ? 'react.development.js' : 'react.production.min.js'
+        }`,
+        `https://file.ljcdn.com/bs/react-dom/17.0.2/umd/${
+          isDevelopment ? 'react-dom.development.js' : 'react-dom.production.min.js'
+        }`,
+        'https://file.ljcdn.com/bs/moment/2.25.3/min/moment.min.js',
+        'https://file.ljcdn.com/bs/antd/4.18.7/dist/antd.min.js'
+      ]
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -8,11 +8,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'antd'
+import { ipcRenderer } from 'electron'
+
+document.title = 'React'
 
 const App = () => {
+  const handleClick = () => {
+    ipcRenderer.send('get-pkg', 'package.json')
+  }
+
+  ipcRenderer.on('get-pkg-reply', (event, arg, arg2) => {
+    console.log(222)
+    console.log(arg)
+    console.log(arg2)
+  })
+
+  const pkg = ipcRenderer.sendSync('sync-get-pkg')
+
+  console.log(333)
+  console.log(pkg)
+
   return (
     <div>
-      <Button type="primary">按钮</Button>
+      <Button type="primary" onClick={handleClick}>
+        按钮
+      </Button>
     </div>
   )
 }

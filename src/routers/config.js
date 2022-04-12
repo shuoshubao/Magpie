04869188ff847062cfb59d375b7a0fc69c3a7ccc
shuoshubao <759979885@qq.com>
2022-04-12 21:08:52 +0800

    feat: 菜单

--- src/routers/config.js
+++ src/routers/config.js
@@ -2,16 +2,20 @@
  * @Author: fangt11
  * @Date:   2021-07-05 16:14:26
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-12 20:44:04
+ * @Last Modified time: 2022-04-12 21:05:46
  * @Desc: 路由配置
  */
 
 import React from 'react'
 import GitlabOutlined from '@ant-design/icons/GitlabOutlined'
 import PictureOutlined from '@ant-design/icons/PictureOutlined'
+import AppstoreOutlined from '@ant-design/icons/AppstoreOutlined'
+import ChromeOutlined from '@ant-design/icons/ChromeOutlined'
 import Index, { NoMatch } from '@/views/Index'
 import Git from '@/views/Git'
 import Tinypng from '@/views/Tinypng'
+import Apps from '@/views/Apps'
+import Browser from '@/views/Browser'
 
 export default [
   {
@@ -19,6 +23,18 @@ export default [
     icon: <GitlabOutlined />,
     path: '/git',
     component: Git
+  },
+  {
+    name: '应用市场',
+    icon: <AppstoreOutlined />,
+    path: '/apps',
+    component: Apps
+  },
+  {
+    name: '浏览器插件',
+    icon: <ChromeOutlined />,
+    path: '/browser',
+    component: Browser
   },
   {
     name: '图片压缩',
---
+++ src/views/Apps/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: shuoshubao
+ * @Date:   2022-04-12 20:59:52
+ * @Last Modified by:   shuoshubao
+ * @Last Modified time: 2022-04-12 21:00:01
+ */
+
+export default () => {
+  return 'Apps'
+}

fd9be328c53f4d976ac3fc69a46b31ecb63637fb
shuoshubao <759979885@qq.com>
2022-04-13 20:48:32 +0800

    feat: npm全局依赖

--- src/views/NodeJs/config.js
+++ src/views/NodeJs/config.js
@@ -2,16 +2,18 @@
  * @Author: shuoshubao
  * @Date:   2022-04-12 14:33:27
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-13 19:56:52
+ * @Last Modified time: 2022-04-13 20:47:45
  */
+import React from 'react'
 import { ipcRenderer } from 'electron'
-import { message } from 'antd'
+import { Tooltip, message } from 'antd'
+import QuestionCircleOutlined from '@ant-design/icons/QuestionCircleOutlined'
 import { rules } from '@nbfe/tools'
 import { RegistryEnum } from '@/configs'
 
 const { required } = rules
 
-export const getColumns = () => {
+export const getFormColumns = () => {
   const { stdout: version } = ipcRenderer.sendSync('execaCommandSync', 'node -v')
   const npmrc = ipcRenderer.sendSync('getNpmrc')
   return [
@@ -37,6 +39,52 @@ export const getColumns = () => {
         onChange: value => {
           ipcRenderer.sendSync('execaCommandSync', `npm config set registry ${value}`)
           message.success(`registry 已切换至: ${value}`)
+        }
+      }
+    }
+  ]
+}
+
+export const getTableColumns = () => {
+  return [
+    {
+      title: 'npm 依赖',
+      dataIndex: 'name',
+      render: (value, record) => {
+        const { registry } = record
+        return (
+          <>
+            <span>{value}</span>
+            <Tooltip title={['registry', registry].join(':')} overlayStyle={{ maxWidth: 400 }}>
+              <QuestionCircleOutlined style={{ marginLeft: 4, color: '#00000073' }} />
+            </Tooltip>
+          </>
+        )
+      }
+    },
+    {
+      title: '当前版本',
+      dataIndex: 'version'
+    },
+    {
+      title: '最新版本',
+      dataIndex: 'latestVersion'
+    },
+    {
+      title: '操作',
+      width: 110,
+      template: {
+        tpl: 'link',
+        render: (value, record) => {
+          return [
+            {
+              text: '升级'
+            },
+            {
+              text: '卸载',
+              danger: true
+            }
+          ]
         }
       }
     }
--- src/views/NodeJs/index.js
+++ src/views/NodeJs/index.js
@@ -2,26 +2,82 @@
  * @Author: shuoshubao
  * @Date:   2022-04-07 21:05:13
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-13 19:41:37
+ * @Last Modified time: 2022-04-13 20:48:18
  */
 import React, { useRef, useState, useEffect } from 'react'
-import { Button } from 'antd'
+import { ipcRenderer } from 'electron'
+import { Button, Modal } from 'antd'
 import Form from '@ke/form'
-import { getColumns } from './config'
+import Table from '@ke/table'
+import { getFormColumns, getTableColumns } from './config'
 
 export const Index = () => {
-  const formRef = useRef()
+  const tableRef = useRef()
+  const [globalDependencies, setGlobalDependencies] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+
+  useEffect(() => {
+    const { stdout } = ipcRenderer.sendSync('execaCommandSync', 'npm list -g --depth 0 --json')
+    const { dependencies } = JSON.parse(stdout)
+    const dataSource = Object.entries(dependencies).reduce((prev, [k, v]) => {
+      if (k === 'npm') {
+        return prev
+      }
+      const { version, resolved } = v
+      const registry = resolved.slice(0, resolved.indexOf('/', 10))
+      prev.push({
+        name: k,
+        version,
+        registry
+      })
+      return prev
+    }, [])
+    setGlobalDependencies(dataSource)
+    tableRef.current.search()
+  }, [setGlobalDependencies])
 
   return (
-    <Form
-      ref={formRef}
-      columns={getColumns()}
-      formProps={{ layout: 'horizontal' }}
-      showResetBtn={false}
-      cardProps={{
-        title: 'Node 全局配置'
-      }}
-    />
+    <>
+      <Form
+        columns={getFormColumns()}
+        formProps={{ layout: 'horizontal' }}
+        showResetBtn={false}
+        cardProps={{
+          title: 'Node 全局配置'
+        }}
+      />
+      <Table
+        ref={tableRef}
+        rowKey="name"
+        columns={getTableColumns()}
+        remoteConfig={{
+          fetch: async () => {
+            return {
+              list: globalDependencies
+            }
+          }
+        }}
+        extraConfig={{ showTotal: true }}
+        pagination={false}
+        appendHeader={
+          <Button
+            type="primary"
+            onClick={() => {
+              setModalVisible(true)
+            }}
+          >
+            添加依赖
+          </Button>
+        }
+      />
+      <Modal
+        title="添加依赖"
+        visible={modalVisible}
+        onCancel={() => {
+          setModalVisible(false)
+        }}
+      ></Modal>
+    </>
   )
 }
 

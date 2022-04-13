5fac0bcaa57ed995b3f19764a4afa57c4ccb9aad
shuoshubao <759979885@qq.com>
2022-04-13 16:27:44 +0800

    feat: 获取 os 信息

--- main/listener.js
+++ main/listener.js
@@ -2,12 +2,15 @@
  * @Author: shuoshubao
  * @Date:   2022-04-12 20:31:01
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-12 20:45:25
+ * @Last Modified time: 2022-04-13 16:11:37
  */
 const { ipcMain, dialog } = require('electron')
 const fs = require('fs')
+const os = require('os')
 const execa = require('execa')
 const glob = require('glob')
+const ip = require('ip')
+const { isFunction } = require('lodash')
 
 ipcMain.handle('showOpenDialog', (event, options) => {
   return dialog.showOpenDialog(options)
@@ -22,6 +25,17 @@ ipcMain.on('fs', (event, fsFuncName, args) => {
   event.returnValue = res
 })
 
+ipcMain.handle('os', (event, propName) => {
+  const value = os[propName]
+  if (propName === 'freemem') {
+    return os.freemem()
+  }
+  if (isFunction(value)) {
+    return value()
+  }
+  return value
+})
+
 ipcMain.on('getImageBase64', (event, filePath) => {
   const res = fs.readFileSync(filePath)
   event.returnValue = res.toString('base64')
@@ -33,4 +47,8 @@ ipcMain.on('execaSync', (event, file, args) => {
 
 ipcMain.on('globSync', (event, pattern, options) => {
   event.returnValue = glob.sync(pattern, options)
+})
+
+ipcMain.on('getIp', (event) => {
+  event.returnValue = ip.address()
 })
--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -2,21 +2,43 @@
  * @Author: fangt11
  * @Date:   2021-07-05 16:14:26
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-12 20:44:27
+ * @Last Modified time: 2022-04-13 16:12:08
  */
 
 import React, { useState, useEffect } from 'react'
 import { ipcRenderer } from 'electron'
-import { Result, Button, Typography } from 'antd'
+import { Card, Result, Button, Typography } from 'antd'
+import DesktopOutlined from '@ant-design/icons/DesktopOutlined'
 import { Descriptions } from '@ke/table'
+import filesize from 'filesize'
 
 const { Text, Title } = Typography
 
 // 首页
 const Index = () => {
-  const [dataSource, setDataSource] = useState()
+  const [dataSource1, setDataSource1] = useState()
+  const [dataSource2, setDataSource2] = useState()
 
-  const columns = [
+  const columns1 = [
+    {
+      label: 'username',
+      name: 'username'
+    },
+    {
+      label: 'CPU',
+      name: 'cpus'
+    },
+    {
+      label: 'freemem',
+      name: 'freemem',
+      render: value => {
+        return filesize(value || 0)
+      }
+    },
+    {
+      label: 'IP',
+      name: 'ip'
+    },
     {
       label: '姓名',
       name: 'name'
@@ -36,28 +58,57 @@ const Index = () => {
     }
   ]
 
+  const columns2 = [
+    {
+      label: 'Electron',
+      name: 'electron'
+    },
+    {
+      label: 'Chromium',
+      name: 'chrome'
+    },
+    {
+      label: 'Node',
+      name: 'node'
+    }
+  ]
+
   useEffect(() => {
-    const { stdout: name } = ipcRenderer.sendSync('execaSync', 'git', ['config', '--global', 'user.name'])
-    const { stdout: email } = ipcRenderer.sendSync('execaSync', 'git', ['config', '--global', 'user.email'])
-    const { stdout: node_version } = ipcRenderer.sendSync('execaSync', 'node', ['-v'])
-    const { stdout: npm_version } = ipcRenderer.sendSync('execaSync', 'npm', ['-v'])
-    setDataSource({
-      name,
-      email,
-      node_version: node_version.match(/(\d+(\.\d+)*)/)?.[1],
-      npm_version
-    })
-  }, [setDataSource])
+    ;(async () => {
+      const userInfo = await ipcRenderer.invoke('os', 'userInfo')
+      const cpus = await ipcRenderer.invoke('os', 'cpus')
+      const freemem = await ipcRenderer.invoke('os', 'freemem')
+      const { stdout: name } = ipcRenderer.sendSync('execaSync', 'git', ['config', '--global', 'user.name'])
+      const { stdout: email } = ipcRenderer.sendSync('execaSync', 'git', ['config', '--global', 'user.email'])
+      const { stdout: node_version } = ipcRenderer.sendSync('execaSync', 'node', ['-v'])
+      const { stdout: npm_version } = ipcRenderer.sendSync('execaSync', 'npm', ['-v'])
+      setDataSource1({
+        username: userInfo.username,
+        cpus: cpus.length,
+        freemem,
+        ip: ipcRenderer.sendSync('getIp'),
+        name,
+        email,
+        node_version: node_version.match(/(\d+(\.\d+)*)/)?.[1],
+        npm_version
+      })
+      setDataSource2(ipcRenderer.sendSync('getProcessVersions'))
+    })()
+  }, [setDataSource1, setDataSource2])
 
   return (
-    <div style={{ height: '100%', padding: '100px 10px 10px', background: '#fff' }}>
-      <Title level={2} style={{ textAlign: 'center' }}>
-        Magpie
-      </Title>
-      <Title level={4} style={{ textAlign: 'center' }}>
-        前端研发工作台
-      </Title>
-      <Descriptions data={dataSource} columns={columns} />
+    <div style={{ height: '100%', padding: 10 }}>
+      <Card style={{ padding: '100px 0' }}>
+        <Title level={2} style={{ textAlign: 'center' }}>
+          <DesktopOutlined />
+          <span style={{ marginLeft: 10 }}>Magpie</span>
+        </Title>
+        <Title level={4} style={{ textAlign: 'center' }}>
+          前端研发工作台
+        </Title>
+      </Card>
+      <Descriptions title="基本信息" data={dataSource1} columns={columns1} style={{ padding: 10, marginTop: 10 }} />
+      <Descriptions title="Electron" data={dataSource2} columns={columns2} style={{ padding: 10, marginTop: 10 }} />
     </div>
   )
 }

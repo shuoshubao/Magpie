3e3a49fd1ca9cdc18335cd2b094f7a001ff6e42e
shuoshubao <759979885@qq.com>
2022-04-14 13:37:08 +0800

    feat: log execaCommandSync

--- main/util.js
+++ main/util.js
@@ -2,11 +2,13 @@
  * @Author: shuoshubao
  * @Date:   2022-04-14 12:18:48
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-14 13:21:17
+ * @Last Modified time: 2022-04-14 13:23:52
  */
+const log = require('electron-log')
 const execa = require('execa')
 
 const execaCommandSync = command => {
+  log.info('execaCommandSync', command)
   const [file, ...args] = command.split(/\s+/)
   return execa.sync(file, args)
 }

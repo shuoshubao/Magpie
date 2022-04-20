caa62da7334b04033130199c92ba3588797a8444
shuoshubao <759979885@qq.com>
2022-04-20 14:41:32 +0800

    feat: app store 布局调整;table变list

--- src/views/Apps/index.js
+++ src/views/Apps/index.js
@@ -2,14 +2,73 @@
  * @Author: shuoshubao
  * @Date:   2022-04-12 20:59:52
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-14 18:33:33
+ * @Last Modified time: 2022-04-20 14:39:35
  */
 import React, { useRef, useState, useEffect } from 'react'
-import Table from '@ke/table'
+import { ipcRenderer, shell } from 'electron'
+import { List, Card, Button, Tag, Tooltip } from 'antd'
+import { presetPalettes } from '@ant-design/colors'
+import CloudDownloadOutlined from '@ant-design/icons/CloudDownloadOutlined'
+import { random } from 'lodash'
 import { getTableColumns, getAppList } from './config'
+
+const colors = Object.keys(presetPalettes)
 
 export default () => {
   return (
-    <Table rowKey="name" columns={getTableColumns()} dataSource={getAppList()} showHeader={false} pagination={false} />
+    <Card>
+      <List
+        rowKey="id"
+        grid={{ gutter: 10, column: 4 }}
+        dataSource={getAppList()}
+        renderItem={item => {
+          const { name, id, icon, downloadUrl, description, categories, installed } = item
+          let extraNode
+          if (installed) {
+            extraNode = <Tag color="success">已安装</Tag>
+          } else {
+            extraNode = (
+              <Button
+                size="small"
+                danger
+                type="primary"
+                icon={<CloudDownloadOutlined />}
+                onClick={() => {
+                  shell.openExternal(downloadUrl)
+                }}
+              >
+                去下载
+              </Button>
+            )
+          }
+          return (
+            <List.Item>
+              <Card title={name} extra={extraNode}>
+                <div style={{ display: 'flex' }}>
+                  <img style={{ width: 60, height: 60 }} src={icon} />
+                  <div style={{ paddingLeft: 5 }}>
+                    <div>{description}</div>
+                    <div>
+                      {categories.map((v, i) => {
+                        const color = presetPalettes[colors[random(colors.length - 1)]].primary
+                        return (
+                          <Tag
+                            color={color}
+                            key={i}
+                            style={{ marginLeft: i === 0 ? 0 : 4, lineHeight: '14px', fontSize: 11 }}
+                          >
+                            {v}
+                          </Tag>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </List.Item>
+          )
+        }}
+      />
+    </Card>
   )
 }

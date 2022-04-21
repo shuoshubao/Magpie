a2bc6a85c9dcacc90eeb56ad0dde35ac4b9c5da5
shuoshubao <759979885@qq.com>
2022-04-21 18:00:57 +0800

    feat: 初始化默认值

--- main/store.js
+++ main/store.js
@@ -2,33 +2,30 @@
  * @Author: shuoshubao
  * @Date:   2022-04-11 14:37:07
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-21 14:53:31
+ * @Last Modified time: 2022-04-21 17:58:17
  * @Desc 配置
  */
 const { app, ipcMain } = require('electron')
 const Store = require('electron-store')
 const { STORE_CONFIG_NAME, prettierConfig } = require('./config')
 
-Store.initRenderer()
-
 const store = new Store({
   name: STORE_CONFIG_NAME
 })
 
-if (!store.get('theme')) {
-  store.set({ theme: 'light' })
+const defaultStore = {
+  theme: 'light', // 主题
+  prettierConfig, // prettier配置
+  defaultPath: app.getPath('downloads') // 默认路径
 }
 
-if (!store.get('prettierConfig')) {
-  store.set({ prettierConfig })
-}
-
-// 默认路径
-if (!store.get('defaultPath')) {
-  store.set({
-    defaultPath: app.getPath('downloads')
-  })
-}
+// 初始化默认值
+Object.entries(defaultStore).forEach(([k, v]) => {
+  if (store.has(k)) {
+    return
+  }
+  store.set(k, v)
+})
 
 ipcMain.on('getStore', (event, name) => {
   event.returnValue = store.get(name)

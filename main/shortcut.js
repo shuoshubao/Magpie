09d55f08436eafe71ea46f2b23ec8d77931eac91
shuoshubao <759979885@qq.com>
2022-04-22 13:52:25 +0800

    feat: cmd+q

--- main/shortcut.js
+++ main/shortcut.js
@@ -2,11 +2,15 @@
  * @Author: shuoshubao
  * @Date:   2022-04-16 23:23:18
  * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-21 13:14:55
+ * @Last Modified time: 2022-04-22 13:51:38
  * @Desc 快捷键
  */
-const { globalShortcut, BrowserWindow } = require('electron')
+const { globalShortcut, app, BrowserWindow } = require('electron')
 
 globalShortcut.register('Command+,', () => {
   BrowserWindow.getAllWindows()[0].webContents.send('showPreferencesModal')
+})
+
+globalShortcut.register('Command+Q', () => {
+  app.quit()
 })

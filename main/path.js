a0a9adddaacbd15c4313dd5bad88a4c0c8d91521
shuoshubao <759979885@qq.com>
2022-06-27 13:42:57 +0800

    fix: path

--- main/path.js
+++ main/path.js
@@ -2,11 +2,11 @@
  * @Author: shuoshubao
  * @Date:   2022-04-15 18:19:22
  * @Last Modified by:   fangt11
- * @Last Modified time: 2022-04-27 17:15:35
+ * @Last Modified time: 2022-06-01 11:18:11
  * @Desc 路径相关处理
  */
 const { app, ipcMain } = require('electron')
-const fs = require('fs')
+const path = require('path')
 const glob = require('glob')
 
 // https://www.electronjs.org/docs/latest/api/app#appgetpathname
@@ -15,7 +15,7 @@ ipcMain.on('getPath', (event, name) => {
 })
 
 ipcMain.on('path', (event, pathFuncName, ...args) => {
-  event.returnValue = fs[pathFuncName](...args)
+  event.returnValue = path[pathFuncName](...args)
 })
 
 ipcMain.on('globSync', (event, pattern, options) => {

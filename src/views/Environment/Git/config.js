6ba44c8f0f9074eedecec6d99597b2c264909f71
shuoshubao <759979885@qq.com>
2022-06-27 13:42:56 +0800

    feat: 用户配置读取

--- src/views/Environment/Git/config.js
+++ src/views/Environment/Git/config.js
@@ -1,15 +1,15 @@
 /*
  * @Author: shuoshubao
  * @Date:   2022-04-12 14:33:27
- * @Last Modified by:   shuoshubao
- * @Last Modified time: 2022-04-13 19:51:26
+ * @Last Modified by:   fangt11
+ * @Last Modified time: 2022-05-31 17:35:36
  */
 import { ipcRenderer } from 'electron'
 import { rules } from '@nbfe/tools'
 
 const { required } = rules
 
-export const getColumns = () => {
+export const getGlobalGitConfigColumns = () => {
   const name = ipcRenderer.sendSync('execaCommandSync', 'git config --global user.name')
   const email = ipcRenderer.sendSync('execaCommandSync', 'git config --global user.email')
   const ignorecase = ipcRenderer.sendSync('execaCommandSync', 'git config --global core.ignorecase') || 'true'
@@ -48,4 +48,35 @@ export const getColumns = () => {
       }
     }
   ]
+}
+
+export const getCustomerGitConfigColumns = ({ initialValues }) => {
+  return [
+    {
+      label: 'Git服务器域名',
+      name: 'hostName',
+      template: {
+        width: 300
+      }
+    },
+    {
+      label: '用户名',
+      name: 'name',
+      template: {
+        width: 300
+      }
+    },
+    {
+      label: '邮箱',
+      name: 'email',
+      template: {
+        width: 300
+      }
+    }
+  ].map(v => {
+    return {
+      ...v,
+      defaultValue: initialValues[v.name]
+    }
+  })
 }
--- src/views/Environment/Git/index.js
+++ src/views/Environment/Git/index.js
@@ -2,41 +2,95 @@
  * @Author: shuoshubao
  * @Date:   2022-04-07 21:05:13
  * @Last Modified by:   fangt11
- * @Last Modified time: 2022-04-27 17:28:55
+ * @Last Modified time: 2022-05-31 18:04:03
  */
-import React, { useRef } from 'react'
-import { Button } from 'antd'
+import React, { useRef, useState, useEffect } from 'react'
+import { ipcRenderer } from 'electron'
+import { Button, Collapse, Card } from 'antd'
 import Form from '@ke/form'
-import { getColumns } from './config'
+import { map } from 'lodash'
+import { isEmptyArray } from '@nbfe/tools'
+import { getGlobalGitConfigColumns, getCustomerGitConfigColumns } from './config'
+
+const { Panel } = Collapse
 
 export const Index = () => {
   const formRef = useRef()
 
+  const [GitConfigs, setGitConfigs] = useState([])
+
+  useEffect(() => {
+    const { GIT_CONFIG_DIR } = ipcRenderer.sendSync('getMainConfig')
+    console.log(111)
+    console.log(GIT_CONFIG_DIR)
+    const dirs = ipcRenderer.sendSync('globSync', `${GIT_CONFIG_DIR}/.gitconfig-*`)
+    const list = dirs.map(v => {
+      console.log(222)
+      console.log(v)
+      const content = ipcRenderer.sendSync('fs', 'readFileSync', v)
+      console.log(content)
+      const config = ipcRenderer.sendSync('ini', 'parse', content)
+      console.log(config)
+      return {
+        configName: v.replace(`${GIT_CONFIG_DIR}/.gitconfig-`, ''),
+        ...config.user
+      }
+    })
+    setGitConfigs(list)
+  }, [setGitConfigs])
+
   return (
-    <Form
-      ref={formRef}
-      columns={getColumns()}
-      formProps={{ layout: 'horizontal' }}
-      showResetBtn={false}
-      cardProps={{
-        title: 'Git 全局配置',
-        size: 'default',
-        bordered: true,
-        extra: (
-          <Button
-            type="primary"
-            onClick={async () => {
-              const formData = await formRef.current.getFormData()
-              if (!formData) {
-                return
-              }
-            }}
-          >
-            保存
+    <>
+      <Form
+        ref={formRef}
+        columns={getGlobalGitConfigColumns()}
+        formProps={{ layout: 'horizontal' }}
+        showResetBtn={false}
+        cardProps={{
+          title: 'Git 全局配置',
+          size: 'default',
+          bordered: true,
+          extra: (
+            <Button
+              type="primary"
+              onClick={async () => {
+                const formData = await formRef.current.getFormData()
+                if (!formData) {
+                  return
+                }
+              }}
+            >
+              保存
+            </Button>
+          )
+        }}
+      />
+      <Card
+        title="用户配置"
+        extra={
+          <Button type="primary" onClick={() => {}}>
+            新增配置
           </Button>
-        )
-      }}
-    />
+        }
+      >
+        {!isEmptyArray(GitConfigs) && (
+          <Collapse defaultActiveKey={map(GitConfigs, 'configName')}>
+            {GitConfigs.map((v, i) => {
+              const { configName, hostName, name, email } = v
+              return (
+                <Panel header={['配置', configName].join('-')} key={configName}>
+                  <Form
+                    columns={getCustomerGitConfigColumns({ initialValues: v })}
+                    formProps={{ layout: 'horizontal' }}
+                    showResetBtn={false}
+                  />
+                </Panel>
+              )
+            })}
+          </Collapse>
+        )}
+      </Card>
+    </>
   )
 }
 

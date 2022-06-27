5ad674f005572ae6674f4b9cef1ca50fd66560cc
shuoshubao <759979885@qq.com>
2022-06-27 13:43:02 +0800

    fix: colors

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,10 +2,10 @@
  * @Author: fangt11
  * @Date:   2021-11-08 11:52:43
  * @Last Modified by:   fangt11
- * @Last Modified time: 2022-04-26 18:39:32
+ * @Last Modified time: 2022-06-01 17:44:37
  */
 
 export * from './enum'
-export * from './color'
+export * from './colors'
 
 export const isDevelopment = process.env.NODE_ENV === 'development'
